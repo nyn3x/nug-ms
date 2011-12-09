8abc3884c68f4130ea4386bfd6cb7964e6107fb2
henning.eiben@gmail.com <henning.eiben@gmail.com@9839ff58-6286-973a-7902-77e69c88e1c0>
2011-12-09 15:38:08 +0000

    Coding Dojo vom 30.11.2011

---
+++ trunk/Katas/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in FizzBuzz.From1To100())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
